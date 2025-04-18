8aa1aa793f49355d5953a07a43958222f47aaeea
Dixit <drdixit6@gmail.com>
2025-04-18 22:37:56 +0530

    current

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
---vim.g.loaded_python3_provider = 0
-vim.g.python3_host_prog = "C:\\Users\\mail\\scoop\\apps\\python\\current\\python.exe"
+vim.g.loaded_python3_provider = 0
+--vim.g.python3_host_prog = "C:\\Users\\mail\\scoop\\apps\\python\\current\\python.exe"
 
 if vim.g.vscode then
   ------------------
